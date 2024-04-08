1ec970c15488eb7d6d673e68072524783094ff94
virtualka2024 <157579287+virtualka2024@users.noreply.github.com>
2024-04-08 16:09:21 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// Change color on click
+const aboutSection = document.getElementById('about');
+aboutSection.addEventListener('click', function() {
+    this.style.backgroundColor = '#f7931e';
+});
